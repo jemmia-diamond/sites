8c4023c63ac6789fb3aa74f9263025adfd59bdf0
tunglamphandam <lamtungphan8@gmail.com>
2026-06-03 15:44:09 +0700

    chore: test infisical key

--- jemmia-products/src/main.tsx
+++ jemmia-products/src/main.tsx
@@ -3,6 +3,10 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+console.log('--- TEST INFISICAL ---');
+console.log('VITE_TEST_KEY:', import.meta.env.VITE_TEST_KEY);
+console.log('------------------------------');
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
